1658f55eb6d755ea7dc3d6819f0ae2760dc8508f
dlam519 <dlam519@student.ubc.ca>
2025-03-11 20:35:20 -0700

    perf: switch to Feather format with read_feather for faster data loading

--- src/utils/data_loader.py
+++ src/utils/data_loader.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 def load_data():
-    file_path = r"data/processed/Cleaned_CanadianHousePrices.csv"
-    df = pd.read_csv(file_path, encoding='ISO-8859-1').dropna()
+    file_path = r"data/processed/Cleaned_CanadianHousePrices.feather"
+    # df = pd.read_csv(file_path, encoding='ISO-8859-1').dropna()
+    df = pd.read_feather(file_path).dropna()
     return df
