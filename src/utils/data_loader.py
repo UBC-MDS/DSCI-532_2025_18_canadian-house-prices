6991dc3d541520144082a44e405e259b917e589a
dlam519 <dlam519@student.ubc.ca>
2025-03-11 20:57:07 -0700

    refactor: Centralize data loading in data_loader.py to avoid redundancy in filters.py and charts.py

--- src/utils/data_loader.py
+++ src/utils/data_loader.py
@@ -1,7 +1,12 @@
 import pandas as pd
 
+_data = None
+
 def load_data():
-    file_path = r"data/processed/Cleaned_CanadianHousePrices.feather"
-    # df = pd.read_csv(file_path, encoding='ISO-8859-1').dropna()
-    df = pd.read_feather(file_path).dropna()
-    return df
+    global _data
+    if _data is None:
+
+        file_path = r"data/processed/Cleaned_CanadianHousePrices.feather"
+        # _data = pd.read_csv(file_path, encoding='ISO-8859-1').dropna()
+        _data = pd.read_feather(file_path).dropna()
+    return _data
