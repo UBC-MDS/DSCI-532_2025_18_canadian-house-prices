90e0c41ca025deb132f24a7c9b546e86544ea932
dlam519 <dlam519@student.ubc.ca>
2025-03-16 21:53:59 -0700

    fix: fix the overlapping problem between cards

--- src/components/summary_cards.py
+++ src/components/summary_cards.py
@@ -15,44 +15,48 @@ def create_summary_cards():
         - Average Number of Bedrooms
         - Average Number of Bathrooms
         - Price Range
-    """
+        """
     return dbc.Row([
 
-        dbc.Col(
-            dcc.Loading(
-                dbc.Card(
-                    html.Div(id="median-price", className="text-center"),
-                    className="summary-card"
+            dbc.Col(
+                dcc.Loading(
+                    dbc.Card(
+                        html.Div(id="median-price", className="text-center"),
+                        className="summary-card"
+                        )
+                    ),
+                width=SUMMARY_CARD_WIDTH
+                ),
+            dbc.Col(
+                dcc.Loading(
+                    dbc.Card(
+                        html.Div(id="avg-bedrooms", className="text-center"),
+                        className="summary-card"
+                        )
+                    ),
+                width=SUMMARY_CARD_WIDTH
+                ),
+            dbc.Col(
+                dcc.Loading(
+                    dbc.Card(
+                        html.Div(id="avg-bathrooms", className="text-center"),
+                        className="summary-card"
+                        )
+                    ),
+                width=SUMMARY_CARD_WIDTH
+                ),
+            dbc.Col(
+                dcc.Loading(
+                    dbc.Card(
+                        html.Div(id="price-range", className="text-center"),
+                        className="summary-card"
+                        )
+                    ),
+                width=SUMMARY_CARD_WIDTH
                 )
-            ),
-            width=SUMMARY_CARD_WIDTH
-        ),
-        dbc.Col(
-            dcc.Loading(
-                dbc.Card(
-                    html.Div(id="avg-bedrooms", className="text-center"),
-                    className="summary-card"
-                )
-            ),
-            width=SUMMARY_CARD_WIDTH
-        ),
-        dbc.Col(
-            dcc.Loading(
-                dbc.Card(
-                    html.Div(id="avg-bathrooms", className="text-center"),
-                    className="summary-card"
-                )
-            ),
-            width=SUMMARY_CARD_WIDTH
-        ),
-        dbc.Col(
-            dcc.Loading(
-                dbc.Card(
-                    html.Div(id="price-range", className="text-center"),
-                    className="summary-card"
-                )
-            ),
-            width=SUMMARY_CARD_WIDTH
-        )
 
-    ], className="flex-grow-1", style={"height": "5vh"})
+            ], className="flex-grow-1", 
+            style={
+                "height": "5vh",
+            }
+        )
